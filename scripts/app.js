043341bda589e37ab16b465c670f9fd2f0625268
Stanciu Gabriel <gabistanciu24@yahoo.com>
2023-02-17 16:31:32 +0200

    Add function to update the board live to be the size of slider's value

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,42 @@
 const board = document.querySelector(".board");
-const sliderValue = document.getElementById("size-val");
+const showSliderValue = document.getElementById("size-val");
+const currentSliderValue = document.getElementById("slider-size");
+
+currentSliderValue.value = 16;
+//continuously update the value while the slider is clicked and held
+currentSliderValue.addEventListener("mousedown", function (e) {
+  currentSliderValue.addEventListener("mousemove", updateSliderValue);
+  clearBoard();
+});
+
+//update the slider value in paragraph
+function updateSliderValue(e) {
+  showSliderValue.innerHTML =
+    currentSliderValue.value + " x " + currentSliderValue.value;
+  updateBoard(currentSliderValue.value);
+}
+
+//stops updating the value once the mouse is released
+currentSliderValue.addEventListener("mouseup", function (e) {
+  showSliderValue.removeEventListener("mousemove", updateSliderValue);
+});
 
 let boardSize = (size) => {
-  board.style.gridTemplateColumns = "repeat(${size},1fr)";
-  board.style.gridTemplateRows = "repeat(${size},1fr)";
+  board.style.gridTemplateColumns = "repeat(" + size + ",1fr)";
+  board.style.gridTemplateRows = "repeat(" + size + ",1fr)";
 
-  for (let i = 0; i < 16 * 16; i++) {
+  for (let i = 0; i < size * size; i++) {
     let square = document.createElement("div");
     square.style.backgroundColor = "blue";
     board.appendChild(square);
   }
 };
+
+let updateBoard = (size) => {
+  clearBoard();
+  boardSize(size);
+};
+
+function clearBoard() {
+  board.innerHTML = "";
+}
