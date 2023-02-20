37835b1e4f44879b3aa2a8a64fa123b047f67d9a
Stanciu Gabriel <gabistanciu24@yahoo.com>
2023-02-20 15:28:21 +0200

    Fix board Drawing while holding mousedown and add onload function for default size of the board

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,37 @@
+const DEFAULT_SIZE = 16;
+const DEFAULT_MODE = "color";
+
 const board = document.querySelector(".board");
 const showSliderValue = document.getElementById("size-val");
 const currentSliderValue = document.getElementById("slider-size");
+
+let currentMode = DEFAULT_MODE;
+
+let isDrawing = false;
+document.body.onmousedown = () => (isDrawing = true);
+document.body.onmouseup = () => (isDrawing = false);
+
+let boardSize = (size) => {
+  board.style.gridTemplateColumns = "repeat(" + size + ",1fr)";
+  board.style.gridTemplateRows = "repeat(" + size + ",1fr)";
+
+  for (let i = 0; i < size * size; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "white";
+    square.addEventListener("mouseover", drawBoard);
+    square.addEventListener("mousedown", drawBoard);
+    board.appendChild(square);
+  }
+};
+
+let updateBoard = (size) => {
+  clearBoard();
+  boardSize(size);
+};
+
+function clearBoard() {
+  board.innerHTML = "";
+}
 
 currentSliderValue.value = 16;
 //continuously update the value while the slider is clicked and held
@@ -21,22 +52,11 @@ currentSliderValue.addEventListener("mouseup", function (e) {
   showSliderValue.removeEventListener("mousemove", updateSliderValue);
 });
 
-let boardSize = (size) => {
-  board.style.gridTemplateColumns = "repeat(" + size + ",1fr)";
-  board.style.gridTemplateRows = "repeat(" + size + ",1fr)";
-
-  for (let i = 0; i < size * size; i++) {
-    let square = document.createElement("div");
-    square.style.backgroundColor = "blue";
-    board.appendChild(square);
-  }
-};
-
-let updateBoard = (size) => {
-  clearBoard();
-  boardSize(size);
-};
-
-function clearBoard() {
-  board.innerHTML = "";
+function drawBoard(e) {
+  if (e.type === "mouseover" && !isDrawing) return;
+  e.target.style.backgroundColor = "black";
 }
+
+window.onload = () => {
+  boardSize(DEFAULT_SIZE);
+};
